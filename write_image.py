dd701e851bae1eff607c7b6fe59e1884efa3eac5
Ednaordinary <88869424+Ednaordinary@users.noreply.github.com>
2024-07-07 17:51:30 -0600

    more stuff

--- write_image.py
+++ write_image.py
@@ -5,26 +5,34 @@ import base64
 import requests
 import struct
 import numpy
+import random
 from PIL import Image
-
-client = socketio.Client()
 
 omcb_map = requests.get("https://onemillioncheckboxes.com/api/initial-state")
 omcb_map = omcb_map.json()['full_state']
 omcb_map = base64.b64decode(omcb_map.encode() + b"=")
 num = 0
+flip_count = 0
+toggler_on = False
+
+state_setter = time.time()
 
 #image = Image.frombytes("1", (1000, 1000), decode)
 #image.save("omcb.png")
 
-target_image = Image.open("target.png")
-target_image.thumbnail((300, 300))
+target_image = Image.open("target1.png")
+target_image.thumbnail((261, 261))
 target_image = target_image.convert('1')
+target_image.save("target_factored.png")
 target_image = numpy.array(target_image)
-target_location = (500, 40)
+target_location = (469, 70)
+target_image2 = Image.open("target2.png")
+target_image2.thumbnail((194, 300))
+target_image2 = target_image2.convert('1')
+target_image2.save("target2_factored.png")
+target_image2 = numpy.array(target_image2)
+target_location2 = (805, 685)
 start_time = time.time()
-flip_count = 0
-last_save = time.time()
 
 bit_queue = []
 
@@ -42,82 +50,122 @@ def set_bit_database(idxs, bit):
     omcb_bin = omcb_bin[bin_offset:]
     omcb_map = int(omcb_bin, 2).to_bytes(125000, byteorder='big')
 
-@client.event
-def connect_error(data):
-    print("dying:", data)
-    
-@client.event
-def disconnect():
-    print("dying")
-    
-@client.on('batched_bit_toggles')
-def on_batched_bit_toggles(data):
-    global omcb_map
-    set_bit_database(data[0], 1)
-    set_bit_database(data[1], 0)
-    global bit_queue
-    global flip_count
-    local_map = bin(int.from_bytes(omcb_map, byteorder='big'))
-    local_map = list(local_map)
-    if "b" in local_map:
-        local_map = local_map[2:]
-    if bit_queue != []:
-        done = 0
-        while done < 2:
-            if not (local_map[bit_queue[0].idx] != "0") == target_image[bit_queue[0].y][bit_queue[0].x]:
-                client.emit('toggle_bit', {'index': bit_queue[0].idx})
-                done += 1
-                flip_count +=1
-            bit_queue.pop(0)
-            print(len(bit_queue), "|", round(flip_count/(time.time() - start_time), 2), "flips per second")
-    if bit_queue == []:
-        threading.Thread(target=toggler).start()
-    global last_save
-    if time.time() > last_save + 2:
+def make_client():
+    client = socketio.Client()
+
+    @client.event
+    def connect_error(data):
+        print("dying:", data)
+        
+    @client.event
+    def disconnect():
+        print("dying")
+        
+    @client.on('batched_bit_toggles')
+    def on_batched_bit_toggles(data):
+        global omcb_map
+        set_bit_database(data[0], 1)
+        set_bit_database(data[1], 0)
+        global bit_queue
+        global flip_count
+        local_map = bin(int.from_bytes(omcb_map, byteorder='big'))
+        local_map = list(local_map)
+        if "b" in local_map:
+            local_map = local_map[2:]
+        if bit_queue != []:
+            #random.shuffle(bit_queue)
+            done = 0
+            while done < 1:
+                if bit_queue[0].image == 1:
+                    if not (local_map[bit_queue[0].idx] != "0") == target_image[bit_queue[0].y][bit_queue[0].x]:
+                        client.emit('toggle_bit', {'index': bit_queue[0].idx})
+                        done += 1
+                        flip_count +=1
+                elif bit_queue[0].image == 2:
+                    if not (local_map[bit_queue[0].idx] != "0") == target_image2[bit_queue[0].y][bit_queue[0].x]:
+                        client.emit('toggle_bit', {'index': bit_queue[0].idx})
+                        done += 1
+                        flip_count +=1
+                bit_queue.pop(0)
+                print(len(bit_queue), "|", round(flip_count/(time.time() - start_time), 2), "flips per second", "|", len(omcb_map))
+
+    @client.on('full_state')
+    def on_full_state(data):
+        global omcb_map
         global num
-        num += 1
+        print("\nFull state recieved\n")
+        #save the state
+        omcb_map = base64.b64decode(data['full_state'].encode() + b"=")
         image = Image.frombytes("1", (1000, 1000), omcb_map)
         image.save("./images/" + str(num) + ".png")
-        last_save = time.time()
+        print("\nImage saved\n")
+        num += 1
 
-@client.on('full_state')
-def on_full_state(data):
-    global omcb_map
-    #save the state
-    omcb_map = base64.b64decode(data['full_state'].encode() + b"=")
-
-@client.event
-def connect():
-    pass
+    @client.event
+    def connect():
+        print("connected")
+        pass
+    
+    return client
 
 class bit_flip:
-    def __init__(self, x, y, idx): # i dont wanna use a tuple thats scaryyyy
+    def __init__(self, x, y, idx, image): # i dont wanna use a tuple thats scaryyyy
         self.x = x
         self.y = y
         self.idx = idx
+        self.image = image
 
 def toggler():
     global omcb_map
     global bit_queue
-    print("renewing queue")
-    local_map = bin(int.from_bytes(omcb_map, byteorder='big'))
-    local_map = list(local_map)
-    if "b" in local_map:
-        local_map = local_map[2:]
-    #for i in range(200000, 1000000):
-    #    if i % 2:
-    #        if local_map[i] == "0":
-    #            bit_queue.append(i)
-    #    else:
-    #        if local_map[i] == "1":
-    #            bit_queue.append(i)
-    #target image
-    #target_location
-    for y, y1 in enumerate(target_image):
-        for x, x1 in enumerate(y1):
-            if (local_map[((y+target_location[1])*1000)+(x+target_location[0])] != "0") != x1:
-                bit_queue.append(bit_flip(x=x,y=y,idx=((y+target_location[1])*1000)+(x+target_location[0])))
+    global toggler_on
+    if not toggler_on:
+        toggler_on = True
+        print("renewing queue")
+        local_map = bin(int.from_bytes(omcb_map, byteorder='big'))
+        local_map = list(local_map)
+        if "b" in local_map:
+            local_map = local_map[2:]
+        for y, y1 in enumerate(target_image):
+            for x, x1 in enumerate(y1):
+                if (local_map[((y+target_location[1])*1000)+(x+target_location[0])] != "0") != x1:
+                    bit_queue.append(bit_flip(x=x,y=y,idx=((y+target_location[1])*1000)+(x+target_location[0]), image=1))
+        for y, y1 in enumerate(target_image2):
+            for x, x1 in enumerate(y1):
+                if (local_map[((y+target_location2[1])*1000)+(x+target_location2[0])] != "0") != x1:
+                    bit_queue.append(bit_flip(x=x,y=y,idx=((y+target_location2[1])*1000)+(x+target_location2[0]), image=2))
+        toggler_on = False
 
-client.connect('https://onemillioncheckboxes.com/', transports='websocket')
-while True:
-    client.wait()
+def renewer():
+    global state_setter
+    while True:
+        if bit_queue == [] or time.time() > state_setter + 5:
+            print("setting state")
+            omcb_map_tmp = requests.get("https://onemillioncheckboxes.com/api/initial-state")
+            omcb_map_tmp = omcb_map_tmp.json()['full_state']
+            omcb_map = base64.b64decode(omcb_map_tmp.encode() + b"=")
+            state_setter = time.time()
+            if bit_queue == []:
+                threading.Thread(target=toggler).start()
+        time.sleep(0.1)
+
+threading.Thread(target=renewer).start()
+
+def run_client(client):
+    while True:
+        client.connect('https://onemillioncheckboxes.com/', transports='websocket')
+        while client.connected:
+            print("connection loop")
+            time.sleep(5)
+            client.wait()
+        print("disconnected, restarting in 10")
+        time.sleep(10)
+
+client_threads = []
+for i in range(1):
+    client_threads.append(threading.Thread(target=run_client, args=[make_client()]))
+for i in client_threads:
+    i.start()
+    time.sleep(10)
+for i in client_threads:
+    i.join()
