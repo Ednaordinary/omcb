00e99091882cc7d13efc7dc0105f4f61abc57e9d
Ednaordinary <88869424+Ednaordinary@users.noreply.github.com>
2024-07-07 20:56:36 -0600

    animating time!!

--- write_image.py
+++ write_image.py
@@ -15,39 +15,50 @@ num = 0
 flip_count = 0
 toggler_on = False
 
-state_setter = time.time()
-
 #image = Image.frombytes("1", (1000, 1000), decode)
 #image.save("omcb.png")
 
-target_image = Image.open("target1.png")
-target_image.thumbnail((261, 261))
-target_image = target_image.convert('1')
-target_image.save("target_factored.png")
-target_image = numpy.array(target_image)
-target_location = (469, 70)
+target_image_list = []
+for i in range(1, 47): # frame count
+    target_image = Image.open("./frames/" + str(i) + ".png")
+    target_image.thumbnail((243, 243))
+    target_image = target_image.convert('1')
+    #target_image.save("target_factored.png")
+    target_image = numpy.array(target_image)
+    target_image_list.append(target_image)
+target_index = 0
+target_location = (480, 30)
 target_image2 = Image.open("target2.png")
 target_image2.thumbnail((200, 300))
 target_image2 = target_image2.convert('1')
 target_image2.save("target2_factored.png")
 target_image2 = numpy.array(target_image2)
 target_location2 = (800, 680)
-start_time = time.time()
+target_image3 = Image.open("target3.png")
+target_image3.thumbnail((90, 700))
+target_image3 = target_image2.convert('1')
+target_image3.save("target3_factored.png")
+target_image3 = numpy.array(target_image3)
+target_location3 = (730, 70)
 
+start_time = time.time()
 bit_queue = []
+
+def bin_convert(omcb_bin):
+    omcb_bin = omcb_bin[omcb_bin.index("b")+1:]
+    if len(omcb_bin) < 1000000:
+        omcb_bin = ["0"]*(1000000-len(omcb_bin)) + omcb_bin
+    return omcb_bin
 
 def set_bit_database(idxs, bit):
     global omcb_map
     omcb_bin = bin(int.from_bytes(omcb_map, byteorder='big'))
     omcb_bin = list(omcb_bin)
     #bin_offset = 2 if "b" in omcb_bin else 0
-    #for idx in idxs:
-    #    try:
-    #        omcb_bin[idx + bin_offset] = str(bit)
-    #    except:
-    #        pass
+    omcb_bin = bin_convert(omcb_bin)
+    for idx in idxs:
+        omcb_bin[idx] = str(bit)
     omcb_bin = ''.join(omcb_bin)
-    omcb_bin = omcb_bin[omcb_bin.index("b")+1:]
     omcb_map = int(omcb_bin, 2).to_bytes(125000, byteorder='big')
 
 def make_client():
@@ -72,13 +83,16 @@ def make_client():
         local_map = list(local_map)
         #if "b" in local_map:
         #    local_map = local_map[2:]
-        local_map = local_map[local_map.index("b")+1:]
+        local_map = bin_convert(local_map)
+        time.sleep(random.randint(500, 5000)/1000)
         if bit_queue != []:
             #random.shuffle(bit_queue)
             done = 0
             while done < 1:
                 # by duplicating the current bit, the clients don't clash into eachother
-                current_bit = bit_queue[0]
+                try:
+                    current_bit = bit_queue[0]
+                except: return
                 bit_queue.pop(0)
                 if current_bit.image == 1:
                     if not (local_map[current_bit.idx] != "0") == target_image[current_bit.y][current_bit.x]:
@@ -127,7 +141,7 @@ def toggler():
         print("renewing queue")
         local_map = bin(int.from_bytes(omcb_map, byteorder='big'))
         local_map = list(local_map)
-        local_map = local_map[local_map.index("b")+1:]
+        local_map = bin_convert(local_map)
         for y, y1 in enumerate(target_image):
             for x, x1 in enumerate(y1):
                 if (local_map[((y+target_location[1])*1000)+(x+target_location[0])] != "0") != x1:
@@ -136,32 +150,37 @@ def toggler():
             for x, x1 in enumerate(y1):
                 if (local_map[((y+target_location2[1])*1000)+(x+target_location2[0])] != "0") != x1:
                     bit_queue.append(bit_flip(x=x,y=y,idx=((y+target_location2[1])*1000)+(x+target_location2[0]), image=2))
+        for y, y1 in enumerate(target_image3):
+            for x, x1 in enumerate(y1):
+                if (local_map[((y+target_location3[1])*1000)+(x+target_location3[0])] != "0") != x1:
+                    bit_queue.append(bit_flip(x=x,y=y,idx=((y+target_location3[1])*1000)+(x+target_location3[0]), image=2))
         toggler_on = False
         preview_image = numpy.array(Image.frombytes("1", (1000, 1000), omcb_map))
         preview_image2 = numpy.array([[False]*1000]*1000)
-        for bit in bit_queue:
-            preview_image[(bit.idx // 1000)][bit.idx - ((bit.idx // 1000)*1000)] = True if preview_image[(bit.idx // 1000)][bit.idx - ((bit.idx // 1000)*1000)] != True else False
-            preview_image2[(bit.idx // 1000)][bit.idx - ((bit.idx // 1000)*1000)] = True
-        preview_image = Image.fromarray((numpy.array(preview_image)*255).round().astype("uint8").squeeze(), mode="L")
-        preview_image.show()
-        preview_image2 = Image.fromarray((numpy.array(preview_image2)*255).round().astype("uint8").squeeze(), mode="L")
-        preview_image2.show()
+        #for bit in bit_queue:
+        #    preview_image[(bit.idx // 1000)][bit.idx - ((bit.idx // 1000)*1000)] = True if preview_image[(bit.idx // 1000)][bit.idx - ((bit.idx // 1000)*1000)] != True else False
+        #    preview_image2[(bit.idx // 1000)][bit.idx - ((bit.idx // 1000)*1000)] = True
+        #preview_image = Image.fromarray((numpy.array(preview_image)*255).round().astype("uint8").squeeze(), mode="L")
+        #preview_image.show()
+        #preview_image2 = Image.fromarray((numpy.array(preview_image2)*255).round().astype("uint8").squeeze(), mode="L")
+        #preview_image2.show()
 
 def renewer():
     global state_setter
+    global start_time
+    global flip_count
     while True:
-        if bit_queue == [] or time.time() > state_setter + 5:
-            print("setting state")
+        if bit_queue == []:
+            print("starting toggler")
             omcb_map_tmp = requests.get("https://onemillioncheckboxes.com/api/initial-state")
             omcb_map_tmp = omcb_map_tmp.json()['full_state']
             omcb_map = base64.b64decode(omcb_map_tmp.encode() + b"=")
-            state_setter = time.time()
-            if bit_queue == []:
-                print("starting toggler")
-                toggler_thread = threading.Thread(target=toggler)
-                toggler_thread.start()
-                toggler_thread.join()
-        time.sleep(0.1)
+            start_time = time.time()
+            flip_count = 0
+            toggler_thread = threading.Thread(target=toggler)
+            toggler_thread.start()
+            toggler_thread.join()
+        time.sleep(10)
 
 threading.Thread(target=renewer).start()
 
@@ -176,7 +195,7 @@ def run_client(client):
         time.sleep(10)
 
 client_threads = []
-for i in range(10):
+for i in range(1): # be careful, multi client is kinda broken
     client_threads.append(threading.Thread(target=run_client, args=[make_client()]))
 for i in client_threads:
     i.start()
